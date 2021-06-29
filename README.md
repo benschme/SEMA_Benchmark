7cd63dfba459a7a03a194e367f99124207352a91
Benjamin Steffen <benjamin.steffen@students.ffhs.ch>
2021-06-29 23:32:20 +0200

    Translated comments to English, Moved SQLite Scripts

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# SEMA_Benchmark
+# Seminararbeit im Studiengang Informatik FS21
+## Leistungsvergleich von Dateisystemen mittels SQLite
+
+Dieser Benchmark dient zum Vergleich der Leistung auf einem beliebigen Dateisystem unter einem reproduzierbares SQLite Workload.
+
+Das Benchmark Skript ist in zwei Teile gegliedert. Zuerst wird die Datenbank initialisiert, dazu gehört das Erstellen der Datenbank, die Konfiguration der verschiedenen Parameter sowie die Vorbelegung der Datensätze. Im nächsten Abschnitt folgen die drei Leistungsmessungen, die mit drei SQL Statements umgesetzt werden.
+
+- Vorbelegung einer Datenbank mit 100'000 Datensätze
+- Anfügen von Datensätze (1'000 Operationen)
+- Lesen von zufälligen Datensätze (1'000 Operationen)
+- Löschen von zufälligen Datensätze (1'000 Operationen)
+
+Ausgabe ist die Zeitmessung je Run die für 1'000 Operationen aufgewendet wird.
+
+
+Beiträge und Feedback sind willkommen!
--- RunBenchmark.sh
+++ RunBenchmark.sh
@@ -1,49 +1,48 @@
 #!/bin/bash
 
-# Alle Ausgaben werden zusätzlich in die Datei Benchmark_Log_SEMA.txt gespeichert
+# All outputs are additionally stored in the Benchmark_Log_SEMA.txt file
 
-#######################
-# Initialize Database
-#######################
+###################################
+# Section 1: Initialize Database
+###################################
 
-# SQLite Datenbank erstellen und Einstellungen anpassen - Journal- und Synchronous Mode
+# Create SQLite Database and customize settings
 echo -e ".databases\nPRAGMA journal_mode=DELETE;\nPRAGMA synchronous=FULL;\n.exit" >> sqlitecommands.txt
 sqlite3 SEMA_DB.sqlite3 < sqlitecommands.txt
 rm sqlitecommands.txt
 
-# Tabelle erstellen
+# Create Table
 sqlite3 SEMA_DB.sqlite3 < /home/sema/Benchmark/SQLite_Benchmark_DB_Initialize.sql
 
-# Pre-population der Datenbank
+# Pre-population of the database
+echo -e "# PRE-POPULATION SECTION\n\n" >> /home/sema/Benchmark/Benchmark_Log_SEMA.txt
 (time sqlite3 SEMA_DB.sqlite3 < /home/sema/Benchmark/SQLite_Benchmark_DB.sql) >> /home/sema/Benchmark/Benchmark_Log_SEMA.txt 2>&1
 
-# Extents auslesen
+# Read extent count
 filefrag SEMA_DB.sqlite3 >> /home/sema/Benchmark/Benchmark_Log_SEMA.txt
 
-#######################
-# INSERTS Run
-#######################
+###################################
+# Section 2: Benchmark Runs
+###################################
+
+# INSERT Run
 
 echo -e "# INSERTS SECTION\n\n" >> /home/sema/Benchmark/Benchmark_Log_SEMA.txt
 (time sqlite3 SEMA_DB.sqlite3 < /home/sema/Benchmark/SQLite_Benchmark_DB_INSERTS.sql) >> /home/sema/Benchmark/Benchmark_Log_SEMA.txt 2>&1
 filefrag SEMA_DB.sqlite3 >> /home/sema/Benchmark/Benchmark_Log_SEMA.txt
 
-#######################
-# SELECTS Run
-#######################
+# SELECT Run
 
 echo -e "# SELECTS SECTION\n\n" >> /home/sema/Benchmark/Benchmark_Log_SEMA.txt
 (time sqlite3 SEMA_DB.sqlite3 < /home/sema/Benchmark/SQLite_Benchmark_DB_SELECTS.sql) >> /home/sema/Benchmark/Benchmark_Log_SEMA.txt 2>&1
 filefrag SEMA_DB.sqlite3 >> /home/sema/Benchmark/Benchmark_Log_SEMA.txt
 
-#######################
-# DELETES Run
-#######################
+# DELETE Run
 
 echo -e "# DELETES SECTION\n\n" >> /home/sema/Benchmark/Benchmark_Log_SEMA.txt
 (time sqlite3 SEMA_DB.sqlite3 < /home/sema/Benchmark/SQLite_Benchmark_DB_DELETES.sql) >> /home/sema/Benchmark/Benchmark_Log_SEMA.txt 2>&1
 filefrag SEMA_DB.sqlite3 >> /home/sema/Benchmark/Benchmark_Log_SEMA.txt
 
-# Datenbank entfernen
+# CleanUp: Remove database
 rm -f SEMA_DB.sqlite3
 
